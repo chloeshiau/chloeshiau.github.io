ddd9cce881715ae4d5ce7e09d1891dc2d4edd65b
chloeshiau <cwshiau@ucdavis.edu>
2015-04-15 17:33:45 -0700

    dumb

--- myScript.js
+++ myScript.js
@@ -1,8 +1,38 @@
-function background_height() {
-	$('body').height($(window).height());
-}
+// function background_height() {
+// 	$('body').height($(window).height());
+// }
 
-$(document).ready(function() {
-	$(window).bind('resize', background_height);
-	background_height();
-});
+// $(document).ready(function() {
+// 	$(window).bind('resize', background_height);
+// 	background_height();
+// });
+
+
+(function ($) {
+
+	$.fn.photoResize = function (options) {
+
+		var element	= $(this), 
+			defaults = {
+	            bottomSpacing: 10
+			};
+		
+		$('body').load(function () {
+			updatePhotoHeight();
+
+			$(window).bind('resize', function () {
+				updatePhotoHeight();
+			});
+		});
+
+		options = $.extend(defaults, options);
+
+		function updatePhotoHeight() {
+			var o = options, 
+				photoHeight = $(window).height();
+
+			$(element).attr('height', photoHeight - o.bottomSpacing);
+		}
+	};
+
+}(jQuery));
