6302be78b3421cbe78fd3c1fd39a4be8d537e3ec
chloeshiau <chloe.shiau@gmail.com>
2015-07-11 12:49:51 -0700

    f

--- myScript.js
+++ myScript.js
@@ -36,21 +36,22 @@
 
 
 $(window).scroll(function() {
-    console.log($(this).scrollTop());
     if ($(this).scrollTop() > 150 && !$('.navigation').hasClass('visible')) {
-        $('.navigation').animate({opacity : 1}, 'slow', function() {
-            $(this).addClass('visible').removeAttr('style');
-        });
-        $('.navigation2').animate({opacity : 1}, 'slow', function() {
-            $(this).addClass('visible').removeAttr('style');
-        });
+        // $('.navigation').animate({opacity : 1}, 'slow', function() {
+        //     $(this).addClass('visible').removeAttr('style');
+        // });
+        // $('.navigation2').animate({opacity : 1}, 'slow', function() {
+        //     $(this).addClass('visible').removeAttr('style');
+        // });
+		$('.navigation').css("opacity","1");
     } else if ($(this).scrollTop() <= 150 && $('.navigation').hasClass('visible')) {
-        $('.navigation').animate({opacity : 0}, 'slow', function() {
-            $(this).removeClass('visible').removeAttr('style');
-        });
-        $('.navigation2').animate({opacity : 0}, 'slow', function() {
-            $(this).addClass('visible').removeAttr('style');
-        });
+        // $('.navigation').animate({opacity : 0}, 'slow', function() {
+        //     $(this).removeClass('visible').removeAttr('style');
+        // });
+        // $('.navigation2').animate({opacity : 0}, 'slow', function() {
+        //     $(this).addClass('visible').removeAttr('style');
+        // });
+		$('.navigation').css("opacity","0");
     }
 });
 
