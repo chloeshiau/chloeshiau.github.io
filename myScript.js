461851c86074f4bffce53873308d634c8dc8f015
chloeshiau <chloe.shiau@gmail.com>
2015-07-11 13:46:49 -0700

    f

--- myScript.js
+++ myScript.js
@@ -36,40 +36,40 @@
 
 
 
-document.getElementById("navhome").onclick = function() {
-	$('html, body').animate({
-		scrollTop:0}, 'slow');
-};
+// document.getElementById("navhome").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:0}, 'slow');
+// };
 
-document.getElementById("navabout").onclick = function() {
-	$('html, body').animate({
-		scrollTop:$('#about').offset().top
-	} , 'slow');
-};
+// document.getElementById("navabout").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:$('#about').offset().top
+// 	} , 'slow');
+// };
 
-document.getElementById("navexp").onclick = function() {
-	$('html, body').animate({
-		scrollTop:$('#experience').offset().top
-	} , 'slow');
-};
+// document.getElementById("navexp").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:$('#experience').offset().top
+// 	} , 'slow');
+// };
 
-document.getElementById("navorg").onclick = function() {
-	$('html, body').animate({
-		scrollTop:$('#organizations').offset().top
-	} , 'slow');
-};
+// document.getElementById("navorg").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:$('#organizations').offset().top
+// 	} , 'slow');
+// };
 
-document.getElementById("navproj").onclick = function() {
-	$('html, body').animate({
-		scrollTop:$('#projects').offset().top
-	} , 'slow');
-};
+// document.getElementById("navproj").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:$('#projects').offset().top
+// 	} , 'slow');
+// };
 
-document.getElementById("navcont").onclick = function() {
-	$('html, body').animate({
-		scrollTop:$('#contactme').offset().top
-	} , 'slow');
-};
+// document.getElementById("navcont").onclick = function() {
+// 	$('html, body').animate({
+// 		scrollTop:$('#contactme').offset().top
+// 	} , 'slow');
+// };
 
 
 //-------------project slides ---------------//
