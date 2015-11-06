0c8298e9b8c3c1af2eb47df3a97180cad2ef2d00
chloeshiau <chloe.shiau@gmail.com>
2015-11-06 12:16:58 -0800

    f

--- js/myScript.js
+++ js/myScript.js
@@ -60,10 +60,12 @@ document.onreadystatechange = function () {
   var state = document.readyState
   if (state == 'interactive') {
        document.getElementById('everything').style.visibility="hidden";
+       document.getElementsByClassName('intro').style.visibility="hidden";
   } else if (state == 'complete') {
       setTimeout(function(){
          document.getElementById('interactive');
          document.getElementById('load').style.visibility="hidden";
+         document.getElementsByClassName('intro').style.visibility="visible";
          document.getElementById('everything').style.visibility="visible";
       },1000);
   }
