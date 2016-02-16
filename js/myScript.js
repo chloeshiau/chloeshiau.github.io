0efcbf193259fa3be0e759be5503810e18953e48
chloeshiau <chloe.shiau@gmail.com>
2016-02-16 15:29:39 -0800

    contact

--- js/myScript.js
+++ js/myScript.js
@@ -126,3 +126,16 @@ function popup(windowname) {
 	toggle('blanket');
 	toggle(windowname);		
 }
+
+
+
+
+var tabs = $('#tabs-titles li'); //grab tabs
+var contents = $('#tabs-contents li'); //grab contents
+
+tabs.bind('click',function(){
+  contents.hide(); //hide all contents
+  tabs.removeClass('current'); //remove 'current' classes
+  $(contents[$(this).index()]).show(); //show tab content that matches tab title index
+  $(this).addClass('current'); //add current class on clicked tab title
+});
